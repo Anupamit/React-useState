16732b6061779202a5193eae3c39fcb00576bb9e
Anupam kumar <anupamit011@gmail.com>
2022-07-02 11:56:17 +0530

    useing useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount]= useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Count is: {count}</p>
+      <div className='button'>
+        <button className='btn1' onClick={()=>setCount(count+1)}>Add 1</button>
+        <button className='btn2' onClick={()=>setCount(count-1)}>Decrease 1</button>
+        <button className='btn3'  onClick={()=>setCount(count+20)}>Add 20</button>
+        <button className='btn4'  onClick={()=>setCount(count-5)}>Decrease 5</button>
+        <button className='btn5' onClick={()=>setCount(0)}>Reset</button>
+      </div>
     </div>
   );
 }
